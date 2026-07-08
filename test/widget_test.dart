39af72cb85b6fd743eb67d49b9fc0033e438234d
L <bladeheaven54@gmail.com>
2026-07-08 11:43:44 -0400

    actualizacion de archivos faltantes
    error en el codigo que solucine

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:prueba_1/main.dart';
+import 'package:hola_word/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MainApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
