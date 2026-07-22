ceb5d1df3fe17145abc23b040cfcd33d845cf875
L <bladeheaven54@gmail.com>
2026-07-22 18:25:21 -0400

    mas ajustes con respecto a la finalizacion del rey
    un gran problema q me falta por resolver

--- lib/game_board.dart
+++ lib/game_board.dart
@@ -380,6 +380,27 @@ class _GameBoardstate extends State<GameBoard> {
     }
     return candidateMoves;
   }
+  // CALCULA MOVIENTO VALIDO
+  List<List<int>> calculateRealValidMoves(int row. int col, ChessPiece? piece; bool checkSimulation){
+    List<List<int>> realValidMoves = [];
+    List<List<int>> candidateMoves = calculateRawValidMoves(row, col, piece);
+
+    // alter generating all los movimientos validos, vamos a filtrar cualquiera que resulte en un jaque del Rey
+    if(checkSimulation){
+      for(var move in  candidateMoves){
+        int endRow = move[0];
+        int endCol = move[1];
+
+        //simula un futuro movimiento si es seguro
+        if(simulateMovesIsSafe()){
+          realValidMoves.add(move);
+        }
+      }
+    } else{
+      realValidMoves = candidateMoves;
+    }
+    return realValidMoves;
+  }
 
   //MOMENTO DE LA CAPACIDAD DE MOVER PIEZAS
   void movePiece(int newRow, int newCol) {
@@ -393,6 +414,17 @@ class _GameBoardstate extends State<GameBoard> {
         blackPiecesTakes.add(capturedPiece);
       }
     }
+
+    //jaque si la pieza verifica q la que se mueve es un rey
+    if (selectedPiece!.type == ChessPieceType.king) {
+      //actualizar la propiedades del rey
+      if (selectedPiece!.isWhite) {
+        whiteKingPosition = [newRow, newCol];
+      } else {
+        blackKingPosition = [newRow, newCol];
+      }
+    }
+
     //movemos la pieza y se borra en el lugar anterior
     board[newRow][newCol] = selectedPiece;
     board[selectedRow][selectedCol] = null;
@@ -447,6 +479,48 @@ class _GameBoardstate extends State<GameBoard> {
     return false;
   }
 
+// SIMULA UN FUTURO MOVIMIENTO PARA VER SI ES SEGURO(NO PONE A NUESTRO PROPIO REY BAJO TAQUE)
+bool simulatedMoveIsSafe(ChessPiece piece, int startRow, int startCol, int endRow, int endCol){
+
+//save the current board state
+
+ ChessPiece? originalDestinationPiece = board[endRow][endCol];
+
+//if the ppiece is the king, save its curret position and update to the new one
+List<int>?originalKingPosition;
+if(piece.type == ChessPieceType.king){
+originalKingPosition = piece.isWhite ? whiteKingPosition : blackKingPosition;
+
+//update the king position
+if(piece.isWhite){
+  whiteKingPosition =[endRow, endCol];
+} else{
+  blackKingPosition =[endRow, endCol];
+ }
+}
+
+//Simulate the move
+board[endRow][endCol] = piece;
+board[startRow][startCol] = null;
+
+//check if our king is under attack
+bool kingInCheck = isKingInCheck(piece.isWhite);
+
+//restore board to original state
+board[startRow][startCol] = piece;
+board[endRow][endCol] = originalDestinationPiece;
+
+//if the piece was the king, restore it original position
+if(piece.type == ChessPieceType.king){
+  if(piece.isWhite){
+    whiteKingPosition = originalKingPosition!;
+  } else{
+    blackKingPosition = originalKingPosition!;
+  }
+}
+// if king is in check = true, means its not a safe move. safe move = false
+return kingInCheck;
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
